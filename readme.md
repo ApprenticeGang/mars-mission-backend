e2324919e09e089037c9bd7a70d2647c39394e79
George <george.thomas@zoopla.co.uk>
2020-09-11 15:34:23 +0100

    added database

--- readme.md
+++ readme.md
@@ -6,3 +6,4 @@ Start by getting an API key from the [NASA API](https://api.nasa.gov/)
 ### Environment variables
 Create a `.env` file in the root of the project, add the following environment variables: 
 - NASA_API_KEY: Your API key
+- DATABASE_URL: postgres://username:password@localhost:5432/db-name
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from 'express';
 import {checkNasaApi} from './nasa/nasaApi';
+import { checkDatabaseConnection } from "./database/database";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,9 +9,8 @@ const port = process.env.PORT || 3000;
 app.get('', async(request, response) => {
     response.json({
         "API": "ok",
-        "nasaAPI": await checkNasaApi()? "ok": "error"
-
-
+        "nasaAPI": await checkNasaApi()? "ok": "error",
+        "database": await checkDatabaseConnection()? "ok": "error",
     });
 });
 
