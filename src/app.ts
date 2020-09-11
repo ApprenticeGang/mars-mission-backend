5a0e61903dfc479bf62f8399438483522e5efb26
Humphrey Codrington <humphrey.codrington@gmail.com>
2020-09-11 14:27:13 +0100

    express fitted

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-console.log('Hello World')
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('', (request, response) => {
+    response.send("hello world");
+});
+
+app.listen(port, () => { console.log(`server is running on port ${port}`) });
