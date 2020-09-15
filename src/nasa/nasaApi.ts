f41cf000729ff3d18491e7b0c4692e2d5bb03fe8
Chilli Mahendra <chillilauren@live.co.uk>
2020-09-15 13:34:43 +0100

    Remove express from nasaApi.ts

--- src/nasa/nasaApi.ts
+++ src/nasa/nasaApi.ts
@@ -1,15 +1,11 @@
 import fetch from "node-fetch";
-import express, { Router } from 'express'
 
-
-const app = express();
 const NASA_API_KEY = process.env.NASA_API_KEY;
 
 export const checkNasaApi = async(): Promise<boolean> => {
     const response = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/?api_key=${NASA_API_KEY}`);
     return response.ok;
 }
-
 
 interface RoverImage {
     img_src: string
