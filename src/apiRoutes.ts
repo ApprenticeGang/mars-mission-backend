df17eb1661f666aa35bedd6e0da9545fa582bd02
Louise <louise.gilligan@zoopla.co.uk>
2020-09-21 11:26:08 +0100

    refactor api routes

---
+++ src/apiRoutes.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import "dotenv/config";
+import {getRoverImages} from "./services/nasaService";
+
+
+const router = express.Router()
+
+
+// define the home page route
+router.get("/rovers/:name/images", async (request :any, response: any) => {
+    const roverName = request.params.name;
+    const images = await getRoverImages(roverName);
+    response.json(images);
+});
+
+
+export {router};
